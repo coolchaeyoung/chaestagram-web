45f23c79719cda7f2a4da24bbbc01fa5960350f9
coolchaeyoung <ggodchae@naver.com>
2021-04-15 01:12:13 +0900

    #8.3 Router Setup part One

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <h1>Home</h1>
+          </Route>
+          <Route path="/join" exact>
+            <h1>Join</h1>
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
