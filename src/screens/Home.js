a2610136744acf3ccf74ed528a3fd4bbc9bb1ccd
coolchaeyoung <ggodchae@naver.com>
2021-05-16 19:39:12 +0900

    #11.6 isLiked

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,7 +1,6 @@
 import { gql, useQuery } from "@apollo/client";
 import React from "react";
 import styled from "styled-components";
-import { logUserOut } from "../apollo";
 import { FatText } from "../components/shared";
 import Avatar from "../components/Avatar";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -11,17 +10,20 @@ import {
   faHeart,
   faPaperPlane,
 } from "@fortawesome/free-regular-svg-icons";
+import { faHeart as SolidHeart } from "@fortawesome/free-solid-svg-icons";
 
 const PhotoContainer = styled.div`
   background-color: white;
+  border-radius: 4px;
   border: 1px solid ${(props) => props.theme.borderColor};
-  margin-bottom: 20px;
+  margin-bottom: 60px;
   max-width: 615px;
 `;
 const PhotoHeader = styled.div`
   padding: 15px;
   display: flex;
   align-items: center;
+  border-bottom: 1px solid rgb(239, 239, 239);
 `;
 
 const Username = styled(FatText)`
@@ -29,11 +31,12 @@ const Username = styled(FatText)`
 `;
 
 const PhotoFile = styled.img`
-  width: 100%;
+  min-width: 100%;
+  max-width: 100%;
 `;
 
 const PhotoData = styled.div`
-  padding: 15px;
+  padding: 12px 15px;
 `;
 
 const PhotoActions = styled.div`
@@ -44,6 +47,9 @@ const PhotoActions = styled.div`
   div {
     display: flex;
     align-items: center;
+  }
+  svg {
+    font-size: 20px;
   }
 `;
 
@@ -70,6 +76,7 @@ const FEED_QUERY = gql`
       comments
       createdAt
       isMine
+      isLiked
     }
   }
 `;
@@ -88,6 +95,7 @@ const testData = [
     comments: 33,
     createdAt: "2021-05-15",
     isMine: true,
+    isLiked: true,
   },
   {
     id: 2,
@@ -102,6 +110,7 @@ const testData = [
     comments: 33,
     createdAt: "2021-05-15",
     isMine: true,
+    isLiked: false,
   },
 ];
 
@@ -120,17 +129,20 @@ const Home = () => {
             <PhotoActions>
               <div>
                 <PhotoAction>
-                  <FontAwesomeIcon size={"2x"} icon={faHeart} />
+                  <FontAwesomeIcon
+                    style={{ color: photo.isLiked ? "tomato" : "inherit" }}
+                    icon={photo.isLiked ? SolidHeart : faHeart}
+                  />
                 </PhotoAction>
                 <PhotoAction>
-                  <FontAwesomeIcon size={"2x"} icon={faComment} />
+                  <FontAwesomeIcon icon={faComment} />
                 </PhotoAction>
                 <PhotoAction>
-                  <FontAwesomeIcon size={"2x"} icon={faPaperPlane} />
+                  <FontAwesomeIcon icon={faPaperPlane} />
                 </PhotoAction>
               </div>
               <div>
-                <FontAwesomeIcon size={"2x"} icon={faBookmark} />
+                <FontAwesomeIcon icon={faBookmark} />
               </div>
             </PhotoActions>
             <Likes>
